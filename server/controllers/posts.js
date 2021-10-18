144734adecc15d4d1ee7d6734cc37c81a195ab67
alex <alexhwang01234@gmail.com>
2021-10-18 13:57:17 -0700

    3.  updated posts.js w/ createPost and getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,15 +2,43 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
+import PostMessage from '../models/postMessage.js'
+
 const router = express.Router()
 
 export const getPosts = async(req, res) => {
     try{
+        // * we are retrieving all the messages in the database
         const postMessages = await PostMessage.find()
+        console.log('this is the postMessages from controllers->post.js',postMessages)
+        console.log(postMessages)
         res.status(200).json(postMessage)
     } catch (error){
         res.status(404).json({message:error.message})
     }
+}
+
+export const createPost = async(req,res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try{
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch(error){
+        res.status(409).json({message:error.message})
+    }
+    
+    // const {title,message,selectedFile,creator,tags} = req.body
+
+    // const newPostMessage = new PostMessage({title,mesage,selectedFile,creator,tags})
+    // try{
+    //     await newPostMessage.save()
+    //     res.status(201).json(newPostMessage)
+    // } catch(error){
+    //     res.status(409).json({mesage:error.message})
+    // }
 }
 
 // export const getPost = async(req, res) => {
@@ -23,17 +51,6 @@ export const getPosts = async(req, res) => {
 //     }
 // }
 
-// export const createPost = async(req,res) => {
-//     const {title,message,selectedFile,creator,tags} = req.body
-
-//     const newPostMessage = new PostMessage({title,mesage,selectedFile,creator,tags})
-//     try{
-//         await newPostMessage.save()
-//         res.status(201).json(newPostMessage)
-//     } catch(error){
-//         res.status(409).json({mesage:error.message})
-//     }
-// }
 
 // export const updatePost = async = (req,res) => {
 //     const {id} = req.params;
