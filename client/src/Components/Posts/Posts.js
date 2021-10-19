0babc9dd87d683652784663ecee7186dee2fb87d
alex <alexhwang01234@gmail.com>
2021-10-19 12:26:41 -0700

    9.  included functionality to post and fetch data, created the cards and the css related to the post

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Post from "./Post/Post";
 import useStyles from './styles'
+import {Grid,CircularProgress} from '@material-ui/core'
 // *this allows us to fetch data from the REDUX store
 import { useSelector } from "react-redux";
 
 
-const Posts = () =>{
+const Posts = ({ setCurrentId }) =>{
     // * how do we know that this is called posts?
         // * if you go in index.js and look at export const reducers=combineReducers({posts})
     const posts = useSelector((state)=>state.posts)
@@ -13,15 +14,19 @@ const Posts = () =>{
 
     console.log('these are the posts in posts.js',posts)
 
-
+// *<Grid key={post._id} item xs={12} sm={6} md={6}>; this deals with the sizing for mobile devices
+//*<Post post ={''} setCurrentId={''}/>; as we we iterate through, we will send the indiviaul value of a post to each post component
     return (
-        <>
-            <h1 >Posts</h1>
-            <Post/>
-            <Post/>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+              {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                  <Post post={post} setCurrentId={setCurrentId} />
+                </Grid>
+              ))}
+            </Grid>
+        )
     )
-
 }
 
 export default Posts
